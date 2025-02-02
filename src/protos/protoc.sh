56a7bb35218103cf7aab32d531cf94fc4cde4cc9
BTbackM <felix.blanco@utec.edu.pe>
2025-02-02 00:48:09 -0500

    fix: Updated proto bash script

--- src/protos/protoc.sh
+++ src/protos/protoc.sh
@@ -13,6 +13,7 @@ sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
+mkdir -p ./golang/${SERVICE_NAME}
 # Generate proto files
 protoc \
   --go_out=paths=source_relative:./golang \
